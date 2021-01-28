37f07291fe24838785f7414c34d9b62e9f374f64
Tommy Persson <tommy.persson@gmail.com>
2021-01-28 19:19:57 +0100

    Show Run/Debug actions as line markers

--- src/main/java/se/fortnox/intellij/jbehave/DebugSingleScenarioAction.java
+++ src/main/java/se/fortnox/intellij/jbehave/DebugSingleScenarioAction.java
@@ -3,11 +3,25 @@ package se.fortnox.intellij.jbehave;
 import com.intellij.execution.Executor;
 import com.intellij.execution.executors.DefaultDebugExecutor;
 import com.intellij.icons.AllIcons;
+import com.intellij.openapi.vfs.VirtualFile;
+import org.jetbrains.annotations.NotNull;
 
 public class DebugSingleScenarioAction extends JbehaveSingleScenarioAction {
 
 	public DebugSingleScenarioAction() {
-		super("Debug This Scenario", "Debug single scenario", AllIcons.Actions.StartDebugger);
+		super("Debug This Scenario",
+			"Debug single scenario",
+			AllIcons.Actions.StartDebugger,
+			null,
+			null);
+	}
+
+	public DebugSingleScenarioAction(@NotNull String scenarioName, @NotNull VirtualFile storyFile) {
+		super("Debug '" + formatTrimmedScenarioName(scenarioName) + "'",
+			"Debug single scenario",
+			AllIcons.Actions.StartDebugger,
+			scenarioName,
+			storyFile);
 	}
 
 	@Override
